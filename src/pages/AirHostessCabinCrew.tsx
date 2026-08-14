2ad8069ed0d72238f7ed019ff8d1777763fff4ff
Ace360degree <ashaikh@a360pl.com>
2026-08-14 13:51:01 +0530

    remove gradient effect from mobile view on Air Hostess page

--- src/pages/AirHostessCabinCrew.tsx
+++ src/pages/AirHostessCabinCrew.tsx
@@ -393,8 +393,6 @@ export default function AirHostessCabinCrew() {
             alt="Air Hostess / Cabin Crew & Hospitality Management"
             className="absolute inset-0 h-full w-full object-cover object-[right_top]"
           />
-          {/* Left White Gradient Overlay matching SS 2 */}
-          <div className="absolute inset-0 bg-gradient-to-r from-white via-white/85 via-52% to-transparent z-0" />
 
           {/* Text Overlay Pinned to Left */}
           <div className="absolute inset-y-0 left-0 z-10 px-5 flex flex-col justify-center max-w-[275px]">
